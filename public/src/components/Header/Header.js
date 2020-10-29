c1d29ac44fbb67efc9570f1552e75e75ead6f1d8
Mark Bykhovets <mb@hrsmnt.ru>
2020-10-29 22:57:30 +0300

    container

--- public/src/components/Header/Header.js
+++ public/src/components/Header/Header.js
@@ -10,13 +10,15 @@ export class Header extends Component {
         const data = this.useSelector(store => store.user.userData);
 
         const header = this.create( `
-            <header class="header"> 
-                <div class='header__logo'>
-                    <img class='header__logo_img' src='/src/images/cat.svg' alt='Logo'/>
-                    <p class='header__logo_text'>MoneyCat</p>
-                </div>
-                <div class='header__control'>
-                    <img class='header__control_avatar' src=${data.avatar || '/src/images/avatar.svg'} alt='Aavatar'/>
+            <header class='header'> 
+                <div class="container">
+                    <div class='header__logo'>
+                        <img class='header__logo_img' src='/src/images/cat.svg' alt='Logo'/>
+                        <p class='header__logo_text'>MoneyCat</p>
+                    </div>
+                    <div class='header__control'>
+                        <img class='header__control_avatar' src=${data.avatar || '/src/images/avatar.svg'} alt='Aavatar'/>
+                    </div>
                 </div>
             </header>
         `);
