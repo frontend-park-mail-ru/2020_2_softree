24d625785e34f28c6e0992aad8cc8b4559981813
Mark Bykhovets <mb@hrsmnt.ru>
2020-10-30 02:45:19 +0300

    Header done

--- public/src/components/MainPage/MainPage.js
+++ public/src/components/MainPage/MainPage.js
@@ -34,12 +34,15 @@ export default class MainPage extends Component {
         }
 
         return this.create( `
-        <div>
-            <h2 class='block-title'>Валюты</h2>
-            <div class='rates-wrapper'>
-                ${this.state.rates.length === 0 ? '<h1>Котировки подгружаются...</h1>' : '<Rates></Rates>'}
-            </div> 
-        </div>`, {
+        <div class="page">
+            <div class="container">
+                <h2 class='block-title'>Валюты</h2>
+                <div class='rates-wrapper'>
+                    ${this.state.rates.length === 0 ? '<h1>Котировки подгружаются...</h1>' : '<Rates></Rates>'}
+                </div> 
+            </div>
+        </div>
+        `, {
             Rates: [Rate, this.state.rates]
         });
     }
