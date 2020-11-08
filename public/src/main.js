98bfd5100c60ec419c757df6dbc33bfabaebc066
MuradyanArtem <61541355+MuradyanArtem@users.noreply.github.com>
2020-11-08 23:02:23 +0300

    add webpack server

--- public/src/main.js
+++ public/src/main.js
@@ -8,14 +8,11 @@ import { thunk, logger } from './modules/Softex/middlewares.js';
 import { Message } from './messages/Message.js';
 import Converter from './components/Converter/Converter.js';
 
+import './index.css';
+
 const softer = new Softer();
 softer.connectStore(applyMiddlewares(createStore(rootReducer), logger, thunk));
 
 softer.initApp(document.getElementById('root'), App);
 softer.initApp(document.getElementById('message'), Message);
 softer.initApp(document.getElementById('converter'), Converter);
-
-const socket = io.connect('http://localhost:3000');
-socket.on('browserReload', function () {
-    document.location.reload(true);
-});
