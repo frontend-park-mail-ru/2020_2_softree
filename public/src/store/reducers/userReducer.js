462acd64e58622a7e6536e01cda5f70ee2851989
Mark Bykhovets <mb@hrsmnt.ru>
2020-11-14 07:27:42 +0300

    drop user data

--- public/src/store/reducers/userReducer.js
+++ public/src/store/reducers/userReducer.js
@@ -20,7 +20,7 @@ const userReducer = (state = initState, action) => {
                 userData: { ...state.userData, avatar: action.payload },
             };
         case userTypes.DROP_DATA:
-            return { ...state, userData: { email: null, avatar: null } };
+            return { ...state, userData: { email: null, avatar: null}, loading: false, accounts: [], histories: [] };
         case userTypes.START_LOADING:
             return { ...state, loading: true };
         case userTypes.END_LOADING:
