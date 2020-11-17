d99e313ba0bf824976f3405b473d42ee95fec5bf
Mark Bykhovets <mb@hrsmnt.ru>
2020-11-17 07:10:27 +0300

    refactor diff

--- public/src/modules/Softer/diff.js
+++ public/src/modules/Softer/diff.js
@@ -1,32 +1,57 @@
-const isDate = d => d instanceof Date;
-const isEmpty = o => Object.keys(o).length === 0;
-const isObject = o => o != null && typeof o === 'object';
-const properObject = o => (isObject(o) && !o.hasOwnProperty ? { ...o } : o);
+const isDate = object => object instanceof Date;
+const isEmpty = object => Object.keys(object).length === 0;
+const isObject = object => object != null && typeof object === 'object';
+const properObject = object => (isObject(object) && !object.hasOwnProperty ? { ...object } : object);
 
-export default function diff(lhs, rhs) {
-    if (lhs === rhs) return {}; // Объекты равны, возвращаем пустой объект
-
-    if (!isObject(lhs) || !isObject(rhs)) return rhs; // return updated rhs
-
-    const l = properObject(lhs);
-    const r = properObject(rhs);
-
-    const deletedValues = Object.keys(l).reduce((acc, key) => {
-        return r.hasOwnProperty(key) ? acc : { ...acc, [key]: undefined };
-    }, {});
-
-    if (isDate(l) || isDate(r)) {
-        if (l.valueOf() === r.valueOf()) return {};
-        return r;
+export default function diff(was, become) {
+    if (was === become) {
+        // Объекты равны, возвращаем пустой объект
+        return {};
     }
 
-    return Object.keys(r).reduce((acc, key) => {
-        if (!l.hasOwnProperty(key)) return { ...acc, [key]: r[key] }; // return added r key
+    if (!isObject(was) || !isObject(become)) {
+        // если что-то одно не объект, значит что-то превратилось в другое и возвращаем become;
+        return become;
+    }
 
-        const difference = diff(l[key], r[key]);
+    const properWas = properObject(was);
+    const properBecome = properObject(become);
 
-        if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc; // return no diff
+    if (isDate(properWas) || isDate(properBecome)) {
+        if (properWas.valueOf() === properBecome.valueOf()) return {};
+        return properBecome;
+    }
 
-        return { ...acc, [key]: difference }; // return updated key
-    }, deletedValues);
+    const deletedValues = getDeletedValues(properWas, properBecome);
+
+    const addedValues = getUpdatedValues(properWas,  properBecome);
+
+    return {...addedValues, ...deletedValues};
+}
+
+const getDeletedValues = (was, become) => {
+    return Object.keys(was).reduce((acc, oldKey) => {
+        return become.hasOwnProperty(oldKey) ? acc : { ...acc, [oldKey]: undefined };
+    }, {});
+}
+
+const getUpdatedValues = (was, become) => {
+    return Object.keys(become).reduce((acc, newKey) => {
+
+        if (!was.hasOwnProperty(newKey)) {
+            // Если появилось новое поле. Добавляем в результат
+            return { ...acc, [newKey]: become[newKey] };
+        }
+
+        // Если поле осталось, то считаем разницу рекурсивно
+        const difference = diff(was[newKey], become[newKey]);
+
+        if (isObject(difference) && isEmpty(difference) && !isDate(difference)){
+            // Если ничего не изменилось, то ничего не добавляем
+            return acc;
+        }
+
+        // Поле обновилось. Добавляем результат
+        return { ...acc, [newKey]: difference };
+    }, {});
 }
