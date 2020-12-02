3eed76a46918844fbe9943b55afa242f21b0335f
Mark Bykhovets <mb@hrsmnt.ru>
2020-12-02 16:08:32 +0300

    fix income

--- public/src/components/Profile/Bag/Statistic/Statistic.js
+++ public/src/components/Profile/Bag/Statistic/Statistic.js
@@ -66,7 +66,7 @@ export default class Statistic extends Component {
         <div class="statistic">
           <div class='bag__info'>
             <p>ДОХОД</p>
-            <p>${calc('USD', 'RUB', this.state.income).toFixed(3)} ₽</p>
+            <p>${calc('USD', 'RUB', this.state.income || 0).toFixed(3)} ₽</p>
           </div>
           <div class="period__selector">
             <PeriodSelector/>
